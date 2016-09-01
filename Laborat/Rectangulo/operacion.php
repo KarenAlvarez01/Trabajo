af11b6c47da44a017c5c9aec46fbfad641ca684e
KarenAlvarez01 <karenalvarez657@gmail.com>
2016-09-01 16:21:44 -0600

    practica4

---
+++ Laborat/Rectangulo/operacion.php
@@ -0,0 +1,15 @@
+<?php
+$a=$_REQUEST["base"];
+$b=$_REQUEST["altura"];
+$area;
+
+funtion muiltiplicar ($a, $b){
+	$multiplicar = $a*$b;
+	return $multiplicar;
+}
+
+$a_area=multiplicar($a,$b);
+echo "El resultado es:$r_area";
+
+
+?>
